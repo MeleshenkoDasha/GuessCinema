945a41b7da9c607ff62d65b24650efb387b96ac0
BlackViking <meleshenko-2014@mail.ru>
2023-01-22 17:27:48 +0300

    Correct Layout Main Window

--- GuessCinema/fMainWindow.Designer.cs
+++ GuessCinema/fMainWindow.Designer.cs
@@ -85,7 +85,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(392, 132);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Мелодию";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -98,7 +98,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(392, 132);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Фильм";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
