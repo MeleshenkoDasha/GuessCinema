4c607e0aeb8a2b2743a5f43438e5ca5923f5e37e
BlackViking <meleshenko-2014@mail.ru>
2023-01-23 22:26:08 +0300

    Correct Rules Game

--- GuessCinema/fRules.Designer.cs
+++ GuessCinema/fRules.Designer.cs
@@ -37,6 +37,8 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pctExit)).BeginInit();
             this.SuspendLayout();
@@ -52,28 +54,34 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 91F));
-            this.tableLayoutPanel1.Controls.Add(this.pctExit, 1, 11);
+            this.tableLayoutPanel1.Controls.Add(this.pctExit, 1, 15);
             this.tableLayoutPanel1.Controls.Add(this.label1, 3, 1);
             this.tableLayoutPanel1.Controls.Add(this.label2, 3, 3);
             this.tableLayoutPanel1.Controls.Add(this.label3, 3, 5);
             this.tableLayoutPanel1.Controls.Add(this.label4, 3, 7);
-            this.tableLayoutPanel1.Controls.Add(this.label5, 3, 9);
-            this.tableLayoutPanel1.Controls.Add(this.label6, 3, 11);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 3, 11);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 3, 15);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 3, 9);
+            this.tableLayoutPanel1.Controls.Add(this.label8, 3, 13);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 13;
+            this.tableLayoutPanel1.RowCount = 17;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 23F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 19F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.5F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1366, 768);
             this.tableLayoutPanel1.TabIndex = 0;
@@ -83,9 +91,9 @@
             this.pctExit.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pctExit.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pctExit.Image = ((System.Drawing.Image)(resources.GetObject("pctExit.Image")));
-            this.pctExit.Location = new System.Drawing.Point(16, 649);
+            this.pctExit.Location = new System.Drawing.Point(16, 670);
             this.pctExit.Name = "pctExit";
-            this.pctExit.Size = new System.Drawing.Size(89, 101);
+            this.pctExit.Size = new System.Drawing.Size(89, 78);
             this.pctExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pctExit.TabIndex = 0;
             this.pctExit.TabStop = false;
@@ -95,11 +103,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label1.Font = new System.Drawing.Font("Cambria", 99.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.Font = new System.Drawing.Font("Cambria", 90F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.Color.Red;
             this.label1.Location = new System.Drawing.Point(124, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1239, 176);
+            this.label1.Size = new System.Drawing.Size(1239, 145);
             this.label1.TabIndex = 1;
             this.label1.Text = "Правила игры";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -110,9 +118,9 @@
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label2.Font = new System.Drawing.Font("Cambria", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
             this.label2.ForeColor = System.Drawing.Color.DarkOrange;
-            this.label2.Location = new System.Drawing.Point(124, 190);
+            this.label2.Location = new System.Drawing.Point(124, 159);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(1239, 92);
+            this.label2.Size = new System.Drawing.Size(1239, 57);
             this.label2.TabIndex = 2;
             this.label2.Text = "Добро пожаловать в игру \"Угадай\"!";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -123,12 +131,12 @@
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label3.Font = new System.Drawing.Font("Cambria", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.OliveDrab;
-            this.label3.Location = new System.Drawing.Point(124, 289);
+            this.label3.Location = new System.Drawing.Point(124, 223);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(1239, 92);
+            this.label3.Size = new System.Drawing.Size(1239, 57);
             this.label3.TabIndex = 3;
-            this.label3.Text = "В нашей игре вы можете проверить свои силы в отгадывании мелодий и фильмов.\r\nВ ка" +
-    "ждой мини-игре есть 2 режима: игра со своей медаитекой и тематическая игра.";
+            this.label3.Text = "В нашей игре вы можете проверить свои силы в отгадывании мелодий и фильмов. \r\nВ к" +
+    "аждой мини-игре есть 3 режима: один игрок, два игрока и тематическая игра. ";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
@@ -137,9 +145,9 @@
             this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label4.Font = new System.Drawing.Font("Cambria", 17.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label4.Location = new System.Drawing.Point(124, 388);
+            this.label4.Location = new System.Drawing.Point(124, 287);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(1239, 122);
+            this.label4.Size = new System.Drawing.Size(1239, 92);
             this.label4.TabIndex = 4;
             this.label4.Text = resources.GetString("label4.Text");
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -148,11 +156,11 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.label5.Font = new System.Drawing.Font("Cambria", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label5.Font = new System.Drawing.Font("Cambria", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label5.Location = new System.Drawing.Point(124, 517);
+            this.label5.Location = new System.Drawing.Point(124, 462);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(1239, 122);
+            this.label5.Size = new System.Drawing.Size(1239, 107);
             this.label5.TabIndex = 5;
             this.label5.Text = resources.GetString("label5.Text");
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -163,12 +171,40 @@
             this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label6.Font = new System.Drawing.Font("Cambria", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
             this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.label6.Location = new System.Drawing.Point(124, 646);
+            this.label6.Location = new System.Drawing.Point(124, 667);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(1239, 107);
+            this.label6.Size = new System.Drawing.Size(1239, 84);
             this.label6.TabIndex = 6;
             this.label6.Text = "УДАЧИ!";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label7.Font = new System.Drawing.Font("Cambria", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label7.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.label7.Location = new System.Drawing.Point(124, 386);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(1239, 69);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Второй режим предназначен для тех, кто хочет помериться знаниями в области \r\nкино" +
+    " и музыки с другом. Для ответа первый игрок использует клавишу Ф, а второй - З. " +
+    "\r\n";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label8.Font = new System.Drawing.Font("Cambria", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.Green;
+            this.label8.Location = new System.Drawing.Point(124, 576);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(1239, 84);
+            this.label8.TabIndex = 8;
+            this.label8.Text = resources.GetString("label8.Text");
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // fRules
             // 
@@ -200,5 +236,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
