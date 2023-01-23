e7de3371bbbfdef9d22e398a4ac224f3abf9b1f2
BlackViking <meleshenko-2014@mail.ru>
2023-01-23 07:51:13 +0300

    Change layout menu video game

--- GuessCinema/fMenuVideo.Designer.cs
+++ GuessCinema/fMenuVideo.Designer.cs
@@ -30,9 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMenuVideo));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.btnOurVideo = new System.Windows.Forms.Button();
+            this.btnTwoPlayer = new System.Windows.Forms.Button();
             this.btnThemeVideo = new System.Windows.Forms.Button();
             this.pctExit = new System.Windows.Forms.PictureBox();
+            this.btnOnePlayer = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pctExit)).BeginInit();
             this.SuspendLayout();
@@ -47,34 +48,37 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
-            this.tableLayoutPanel1.Controls.Add(this.btnOurVideo, 4, 1);
-            this.tableLayoutPanel1.Controls.Add(this.btnThemeVideo, 4, 3);
-            this.tableLayoutPanel1.Controls.Add(this.pctExit, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btnTwoPlayer, 4, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnThemeVideo, 4, 5);
+            this.tableLayoutPanel1.Controls.Add(this.pctExit, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.btnOnePlayer, 4, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 6;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1366, 768);
             this.tableLayoutPanel1.TabIndex = 1;
             // 
-            // btnOurVideo
+            // btnTwoPlayer
             // 
-            this.btnOurVideo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnOurVideo.BackgroundImage")));
-            this.btnOurVideo.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnOurVideo.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnOurVideo.Font = new System.Drawing.Font("Cambria", 26.25F, System.Drawing.FontStyle.Bold);
-            this.btnOurVideo.Location = new System.Drawing.Point(547, 310);
-            this.btnOurVideo.Name = "btnOurVideo";
-            this.btnOurVideo.Size = new System.Drawing.Size(403, 147);
-            this.btnOurVideo.TabIndex = 0;
-            this.btnOurVideo.Text = "Своя игра";
-            this.btnOurVideo.UseVisualStyleBackColor = true;
+            this.btnTwoPlayer.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnTwoPlayer.BackgroundImage")));
+            this.btnTwoPlayer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTwoPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnTwoPlayer.Font = new System.Drawing.Font("Cambria", 26.25F, System.Drawing.FontStyle.Bold);
+            this.btnTwoPlayer.Location = new System.Drawing.Point(547, 371);
+            this.btnTwoPlayer.Name = "btnTwoPlayer";
+            this.btnTwoPlayer.Size = new System.Drawing.Size(403, 124);
+            this.btnTwoPlayer.TabIndex = 0;
+            this.btnTwoPlayer.Text = "Своя игра";
+            this.btnTwoPlayer.UseVisualStyleBackColor = true;
             // 
             // btnThemeVideo
             // 
@@ -82,9 +86,9 @@
             this.btnThemeVideo.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnThemeVideo.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnThemeVideo.Font = new System.Drawing.Font("Cambria", 26.25F, System.Drawing.FontStyle.Bold);
-            this.btnThemeVideo.Location = new System.Drawing.Point(547, 501);
+            this.btnThemeVideo.Location = new System.Drawing.Point(547, 524);
             this.btnThemeVideo.Name = "btnThemeVideo";
-            this.btnThemeVideo.Size = new System.Drawing.Size(403, 147);
+            this.btnThemeVideo.Size = new System.Drawing.Size(403, 124);
             this.btnThemeVideo.TabIndex = 1;
             this.btnThemeVideo.Text = "Тематическая игра";
             this.btnThemeVideo.UseVisualStyleBackColor = true;
@@ -101,6 +105,18 @@
             this.pctExit.TabIndex = 2;
             this.pctExit.TabStop = false;
             this.pctExit.Click += new System.EventHandler(this.pctExit_Click);
+            // 
+            // btnOnePlayer
+            // 
+            this.btnOnePlayer.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnOnePlayer.BackgroundImage")));
+            this.btnOnePlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnOnePlayer.Font = new System.Drawing.Font("Cambria", 26.25F, System.Drawing.FontStyle.Bold);
+            this.btnOnePlayer.Location = new System.Drawing.Point(547, 218);
+            this.btnOnePlayer.Name = "btnOnePlayer";
+            this.btnOnePlayer.Size = new System.Drawing.Size(403, 124);
+            this.btnOnePlayer.TabIndex = 3;
+            this.btnOnePlayer.Text = "Один игрок";
+            this.btnOnePlayer.UseVisualStyleBackColor = true;
             // 
             // fMenuVideo
             // 
@@ -124,8 +140,9 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button btnOurVideo;
+        private System.Windows.Forms.Button btnTwoPlayer;
         private System.Windows.Forms.Button btnThemeVideo;
         private System.Windows.Forms.PictureBox pctExit;
+        private System.Windows.Forms.Button btnOnePlayer;
     }
 }
