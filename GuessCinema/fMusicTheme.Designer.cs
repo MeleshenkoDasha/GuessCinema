9589818535612de00eeee11137aeeb5ccfcf4042
BlackViking <meleshenko-2014@mail.ru>
2023-01-23 23:04:12 +0300

    Add Form with theme

---
+++ GuessCinema/fMusicTheme.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace GuessCinema
+{
+    partial class fMusicTheme
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMusicTheme));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.pctExit = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctExit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 9;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.5F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.5F));
+            this.tableLayoutPanel1.Controls.Add(this.button1, 3, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 5, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button3, 7, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button4, 3, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button5, 5, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button6, 7, 3);
+            this.tableLayoutPanel1.Controls.Add(this.button7, 3, 5);
+            this.tableLayoutPanel1.Controls.Add(this.button8, 5, 5);
+            this.tableLayoutPanel1.Controls.Add(this.button9, 7, 5);
+            this.tableLayoutPanel1.Controls.Add(this.pctExit, 1, 7);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 9;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1366, 768);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(131, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(390, 132);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Рок";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(540, 218);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(390, 132);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Десткие песни";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(949, 218);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(390, 132);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Лихиие 90-е";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(131, 363);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(390, 132);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "SoundTracks";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Bold);
+            this.button5.Location = new System.Drawing.Point(540, 363);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(390, 132);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Старые песни о главном";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button6.BackgroundImage")));
+            this.button6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button6.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Bold);
+            this.button6.Location = new System.Drawing.Point(949, 363);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(390, 132);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Фабрика звезд";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button7.BackgroundImage")));
+            this.button7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button7.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Bold);
+            this.button7.Location = new System.Drawing.Point(131, 508);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(390, 132);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "2000-е";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button8.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Bold);
+            this.button8.Location = new System.Drawing.Point(540, 508);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(390, 132);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Патриотические";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button9.BackgroundImage")));
+            this.button9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button9.Font = new System.Drawing.Font("Cambria", 27.75F, System.Drawing.FontStyle.Bold);
+            this.button9.Location = new System.Drawing.Point(949, 508);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(390, 132);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Времена года";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // pctExit
+            // 
+            this.pctExit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pctExit.Image = ((System.Drawing.Image)(resources.GetObject("pctExit.Image")));
+            this.pctExit.Location = new System.Drawing.Point(23, 653);
+            this.pctExit.Name = "pctExit";
+            this.pctExit.Size = new System.Drawing.Size(89, 101);
+            this.pctExit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctExit.TabIndex = 9;
+            this.pctExit.TabStop = false;
+            // 
+            // fMusicTheme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1366, 768);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "fMusicTheme";
+            this.Text = "fMusicTheme";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pctExit)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.PictureBox pctExit;
+    }
+}
