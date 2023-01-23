5836b9dbb9c280a1873b209c06ee803ee0696de6
BlackViking <meleshenko-2014@mail.ru>
2023-01-23 07:45:16 +0300

    Change layout misuc menu

--- GuessCinema/fMenuMusic.Designer.cs
+++ GuessCinema/fMenuMusic.Designer.cs
@@ -30,9 +30,10 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMenuMusic));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.btnOurMusic = new System.Windows.Forms.Button();
+            this.btnTwoPlayers = new System.Windows.Forms.Button();
             this.btnThemeMusic = new System.Windows.Forms.Button();
             this.pctExit = new System.Windows.Forms.PictureBox();
+            this.btnOnePlayer = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pctExit)).BeginInit();
             this.SuspendLayout();
@@ -47,34 +48,37 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
-            this.tableLayoutPanel1.Controls.Add(this.btnOurMusic, 4, 1);
-            this.tableLayoutPanel1.Controls.Add(this.btnThemeMusic, 4, 3);
-            this.tableLayoutPanel1.Controls.Add(this.pctExit, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btnTwoPlayers, 4, 3);
+            this.tableLayoutPanel1.Controls.Add(this.btnThemeMusic, 4, 5);
+            this.tableLayoutPanel1.Controls.Add(this.pctExit, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.btnOnePlayer, 4, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 6;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowCount = 8;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 28F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 1F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(1366, 768);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
-            // btnOurMusic
+            // btnTwoPlayers
             // 
-            this.btnOurMusic.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnOurMusic.BackgroundImage")));
-            this.btnOurMusic.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnOurMusic.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.btnOurMusic.Font = new System.Drawing.Font("Cambria", 26.25F, System.Drawing.FontStyle.Bold);
-            this.btnOurMusic.Location = new System.Drawing.Point(547, 310);
-            this.btnOurMusic.Name = "btnOurMusic";
-            this.btnOurMusic.Size = new System.Drawing.Size(403, 147);
-            this.btnOurMusic.TabIndex = 0;
-            this.btnOurMusic.Text = "Своя игра";
-            this.btnOurMusic.UseVisualStyleBackColor = true;
+            this.btnTwoPlayers.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnTwoPlayers.BackgroundImage")));
+            this.btnTwoPlayers.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnTwoPlayers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnTwoPlayers.Font = new System.Drawing.Font("Cambria", 26.25F, System.Drawing.FontStyle.Bold);
+            this.btnTwoPlayers.Location = new System.Drawing.Point(547, 371);
+            this.btnTwoPlayers.Name = "btnTwoPlayers";
+            this.btnTwoPlayers.Size = new System.Drawing.Size(403, 124);
+            this.btnTwoPlayers.TabIndex = 0;
+            this.btnTwoPlayers.Text = "Два игрока";
+            this.btnTwoPlayers.UseVisualStyleBackColor = true;
             // 
             // btnThemeMusic
             // 
@@ -82,9 +86,9 @@
             this.btnThemeMusic.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnThemeMusic.Dock = System.Windows.Forms.DockStyle.Fill;
             this.btnThemeMusic.Font = new System.Drawing.Font("Cambria", 26.25F, System.Drawing.FontStyle.Bold);
-            this.btnThemeMusic.Location = new System.Drawing.Point(547, 501);
+            this.btnThemeMusic.Location = new System.Drawing.Point(547, 524);
             this.btnThemeMusic.Name = "btnThemeMusic";
-            this.btnThemeMusic.Size = new System.Drawing.Size(403, 147);
+            this.btnThemeMusic.Size = new System.Drawing.Size(403, 124);
             this.btnThemeMusic.TabIndex = 1;
             this.btnThemeMusic.Text = "Тематическая игра";
             this.btnThemeMusic.UseVisualStyleBackColor = true;
@@ -101,6 +105,19 @@
             this.pctExit.TabIndex = 2;
             this.pctExit.TabStop = false;
             this.pctExit.Click += new System.EventHandler(this.pctExit_Click);
+            // 
+            // btnOnePlayer
+            // 
+            this.btnOnePlayer.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnOnePlayer.BackgroundImage")));
+            this.btnOnePlayer.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOnePlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnOnePlayer.Font = new System.Drawing.Font("Cambria", 26.25F, System.Drawing.FontStyle.Bold);
+            this.btnOnePlayer.Location = new System.Drawing.Point(547, 218);
+            this.btnOnePlayer.Name = "btnOnePlayer";
+            this.btnOnePlayer.Size = new System.Drawing.Size(403, 124);
+            this.btnOnePlayer.TabIndex = 3;
+            this.btnOnePlayer.Text = "Один игрок";
+            this.btnOnePlayer.UseVisualStyleBackColor = true;
             // 
             // fMenuMusic
             // 
@@ -124,8 +141,9 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button btnOurMusic;
+        private System.Windows.Forms.Button btnTwoPlayers;
         private System.Windows.Forms.Button btnThemeMusic;
         private System.Windows.Forms.PictureBox pctExit;
+        private System.Windows.Forms.Button btnOnePlayer;
     }
 }
